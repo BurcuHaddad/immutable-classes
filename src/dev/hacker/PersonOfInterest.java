29daeef65bb014f1b2f40639b9bcb8889d1f32bc
BurcuHaddad <burcuhaddad@gmail.com>
2023-10-31 07:08:55 +0300

    exploring defensive code

---
+++ src/dev/hacker/PersonOfInterest.java
@@ -0,0 +1,12 @@
+package dev.hacker;
+
+import dev.bh.PersonImmutable;
+
+public class PersonOfInterest extends PersonImmutable {
+
+    public PersonOfInterest(PersonImmutable person) {
+        super(person);
+    }
+
+
+}
